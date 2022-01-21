bccc4c849fdfbc0fa951ed399dbf1f58bc361cd4
Brian Sukhnandan <brian@Brians-MacBook-Air.local>
2022-01-21 15:37:41 -0500

    Initial version of disassembler, does not account for addressing modes

--- disassembler/disassembler.rs
+++ disassembler/disassembler.rs
@@ -1,7 +1,5 @@
 fn read_opcode(opcode: u8) {
-
     match opcode {
-
         0x00 => println!("BRK impl"),
         0x01 => println!("ORA (indr,X)"),
         0x05 => println!("ORA zpg"),
@@ -88,19 +86,91 @@ fn read_opcode(opcode: u8) {
         0x86 => println!("STX zpg"),
         0x88 => println!("DEY impl"),
         0x8A => println!("TXA impl"),
-        0x8C => println!("STY impl"),
-        0x8D => println!("STA impl"),
-        0x8E => println!("STX impl"),
+        0x8C => println!("STY abs"),
+        0x8D => println!("STA abs"),
+        0x8E => println!("STX abs"),
+
+        0x90 => println!("BCC rel"),
+        0x91 => println!("STA (indr,Y)"),
+        0x94 => println!("STY (zpg,X)"),
+        0x95 => println!("STA (zpg,X)"),
+        0x96 => println!("STX (zpg,Y)"),
+        0x98 => println!("TYA impl"),
+        0x99 => println!("STA (abs,Y)"),
+        0x9A => println!("TXS impl"),
+        0x9D => println!("STA (abs,X)"),
+
+        0xA0 => println!("LDY immediate"),
+        0xA1 => println!("LDA (indr,X)"),
+        0xA2 => println!("LDX immediate"),
+        0xA4 => println!("LDY zpg"),
+        0xA5 => println!("LDA zpg"),
+        0xA6 => println!("LDX zpg"),
+        0xA8 => println!("TAY impl"),
+        0xA9 => println!("LDA immediate"),
+        0xAA => println!("TAX impl"),
+        0xAC => println!("LDY abs"),
+        0xAD => println!("LDA abs"),
+        0xAE => println!("LDX abs"),
+
+        0xB0 => println!("BCS rel"),
+        0xB1 => println!("LDA (indr,Y)"),
+        0xB4 => println!("LDY (zpg,X)"),
+        0xB5 => println!("LDA (zpg,X)"),
+        0xB6 => println!("LDX (zpg,Y)"),
+        0xB8 => println!("CLV impl"),
+        0xB9 => println!("LDA (abs,Y)"),
+        0xBA => println!("TSX impl"),
+        0xBC => println!("LDY (abs,X)"),
+        0xBD => println!("LDA (abs,X)"),
+        0xBE => println!("LDX (abs,Y)"),
+
+        0xC0 => println!("CPY immediate"),
+        0xC1 => println!("CMP (indr,X)"),
+        0xC4 => println!("CPY zpg"),
+        0xC5 => println!("CMP zpg"),
+        0xC6 => println!("DEC zpg"),
+        0xC8 => println!("INY impl"),
+        0xC9 => println!("CMP immediate"),
+        0xCA => println!("DEX impl"),
+        0xCC => println!("CPY abs"),
+        0xCD => println!("CMP abs"),
+        0xCE => println!("DEC abs"),
+
+        0xD0 => println!("BNE rel"),
+        0xD1 => println!("CMP (indr,Y)"),
+        0xD5 => println!("CMP (zpg,X)"),
+        0xD6 => println!("DEC (zpg,X)"),
+        0xD8 => println!("CLD impl"),
+        0xD9 => println!("CMP (abs,Y)"),
+        0xDD => println!("CMP (abs,X)"),
+        0xDE => println!("DEC (abs,X)"),
+
+        0xE0 => println!("CPX immediate"),
+        0xE1 => println!("SBC (indr,X)"),
+        0xE4 => println!("CPX zpg"),
+        0xE5 => println!("SBC zpg"),
+        0xE6 => println!("INC zpg"),
+        0xE8 => println!("INX impl"),
+        0xE9 => println!("SBC immediate"),
+        0xEA => println!("NOP impl"),
+        0xEC => println!("CPX abs"),
+        0xED => println!("SBC abs"),
+        0xEE => println!("INC abs"),
+
+        0xF0 => println!("BEQ rel"),
+        0xF1 => println!("SBC (indr,Y)"),
+        0xF5 => println!("SBC (zpg,X)"),
+        0xF6 => println!("INC (zpg,X)"),
+        0xF8 => println!("SED impl"),
+        0xF9 => println!("SBC (abs,Y)"),
+        0xFD => println!("SBC (abs,X)"),
+        0xFE => println!("INC (abs,X)"),
 
         _ => println!("INVALID/UNIMPLEMENTED OPCODE"),
-
     }
-
 }
 
 fn main() {
-
     read_opcode(0x81);
-
-    println!("End of main");
 }
